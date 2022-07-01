da2e6fea4d7352f6198eda62fd08e7a1b7de9a96
pokkew <brunogabrielpk@gmail.com>
2022-06-30 22:41:33 -0400

    switch from golang to python

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # jirawkreader
 
-Fow now, this is a XML parser that retrieves information from a Jira workflow.xml file e show it on the terminal.
+Fow now, this is a XML parser that retrieves information from a Jira workflow.xml file and generate a representational image without needing to import it on Jira
 
 In the future it may become an web application with visual elements :)
 
 In order to run this prototype, clone this repos and enter the folder.
 Then, run the following code:
 ```
-go run main.go
+python run main1.go
 ```
 Authors:  
 - **Bruno Gabriel Corrêa Pereira**
---
+++ main1.py
@@ -0,0 +1,43 @@
+import xmltodict
+import graphviz
+
+with open('wk4.xml') as fd:
+    doc = xmltodict.parse(fd.read())
+
+dot = graphviz.Digraph(comment='Workflow')
+init_action = doc['workflow']['initial-actions']['action']['@name']
+init_target_action = doc['workflow']['initial-actions']['action']['results']['unconditional-result']['@step']
+dot.node(init_action, init_action, {'color': 'lightblue', 'shape': 'box', 'style': 'filled'})
+
+for status in doc['workflow']['steps']['step']:
+    # print(status['@name'])
+    st_name = status['@name']
+    st_id = status['@id']
+    dot.node(st_id, st_name, {'color': 'lightblue', 'shape': 'box', 'style': 'filled'})
+
+dot.edge(init_action, init_target_action)
+for status in doc['workflow']['steps']['step']:
+    st_id = status['@id']
+    st_target_id =status['actions']['action']['results']['unconditional-result']['@step']
+    dot.edge(st_id, st_target_id)
+
+dot.view()
+
+
+
+# # Create the object
+# dot = graphviz.Graph(comment='Example')
+
+# # add nodes
+# dot.node('db1', 'input A', {'color': 'aquamarine'}, style='filled')
+# dot.node('db2', 'input B', {'color': 'aquamarine'}, style='filled')
+# dot.node('db3', 'input C', {'color': 'aquamarine'}, style='filled')
+
+# dot.node('B', 'transformation', shape='box', style='filled', color='lightblue')
+# dot.node('C', 'output', shape='cylinder', style='filled', color='red')
+
+# # add edges
+# for n in ['db1', 'db2', 'db3']:
+#     dot.edge(n, 'B')
+
+# dot.edge('B', 'C')
