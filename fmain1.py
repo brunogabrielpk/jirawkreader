31a26bd00ec1b285e5f4a8f61e87eff858e1118b
pokkew <brunogabrielpk@gmail.com>
2022-09-08 23:46:49 -0400

    added some description to fmain.py

--- fmain1.py
+++ fmain1.py
@@ -9,43 +9,46 @@ def runxml(fname):
     dot = graphviz.Digraph(comment='Workflow')
     ##################
     ### TODO ###
-    ### 1 => Recognize the type of workflow: JSW or JSM
-    ### 2 => For each workflow, extract all the statuses and define the inital status
-    ### 3 => For each workflow, extract the transitions, the commom ones and the unique to each status
-    ### 4 => Build the workflow image
+    ### [DEPRECATED] 1 => Recognize the type of workflow: JSW or JSM
+    ### [DEPRECATED] 2 => For each workflow, extract all the statuses and define the inital status
+    ### [DEPRECATED] 3 => For each workflow, extract the transitions, the commom ones and the unique to each status
+    ### [DEPRECATED] 4 => Build the workflow image
     ##################
     
     ###################
-    ### Step 1 -  Identify the workflow Status ###
-    ### [DONE] - Substep 1 -> create a folder to store examples of workflows
-    ### Identify the Workflow type 
-    #### IF workflow == JSW ###
-    ####
-    ####
-    ####
-    ##### Extract statuses and initial status
-    ####
-    ####
-    ####
-    ##### Extract unique and commom transitions
-    ####
-    ####
-    ####
-    #### IF workflow == JSM ###
-    ####
-    ####
-    ####
-    ##### Extract statuses and initial status
-    ####
-    ####
-    ####
-    ##### Extract unique and commom transitions
-    ####
-    ####
-    ####
-
-    ### End Step 1 ####
+    ### Option 1 -  Identify the workflow Status ###
+    ### [DEPRECATED][DONE] - Substep 1 -> create a folder to store examples of workflows
+    ### [DEPRECATED] Identify the Workflow type
+    #### [DEPRECATED] What differ JSM from JSW workflows ?
+    #### [DEPRECATED] the only difference I've noticed (for Jira 9.2 local environment) is the presence of a meta attribute on the
+    #### [DEPRECATED] xml file named "gh.version". I was unable to find this meta attribute on the JSM workflow.
+    #### [DEPRECATED] Maybe, it worth to consider making a logic that cover both types of workflows.
+    #### [DEPRECATED] IF not, proceed with the steps below (treat these step)
+    ##### [DEPRECATED] IF workflow == JSW ###
+    ###### [DEPRECATED] Extract statuses and initial status
+    ###### [DEPRECATED] Extract unique and commom transitions
+    ##### [DEPRECATED] IF workflow == JSM ###
+    ###### [DEPRECATED] Extract statuses and initial status
+    ###### [DEPRECATED] Extract unique and commom transitions
+    ### [DEPRECATED] End Option 1 ####
     ###################
+    ### Option 2 - treat both types of workflows as one thing, so I'll need to extract all info
+    ### regardless of the type
+    ### For now, I believe that are 4 types of information present on the workflows
+    ### 2.1 -> Initial actions
+    ### 2.2 -> Global Actions
+    ### 2.3 -> common actions
+    ### 2.4 -> Steps
+    ### also, I believe that not every item of the above list above will be present on every workflow
+    ### So, I should export other workflows from different versions of Jira and check for the incidence of these 4 items
+    ### on the exported workflows.
+    ### TODO - update
+    ### I was able to create and export a custom workflow that have all the 4 items mentioned above, so, we shold work on this
+    #### Getting to work
+    #### TODO 2.1 => Extract the initial actions
+    ####
+    ####
+    ### End Option 2
     ###################
     ### OLD code ###
     # init_action = doc['workflow']['initial-actions']['action']['@name']
